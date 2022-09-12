0bad860036b40e5d33b24dd24a857123e1892820
Sven <sven-s@posteo.de>
2022-09-12 17:18:29 +0200

    added license and changed long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 with open("requirements.txt") as fh:
     requirements = fh.read().splitlines()
 
 setuptools.setup(
     name="utfcolor",
     version="1.0.0",
+    license="MIT",
     author="5omega",
     author_email="5omega@posteo.de",
     description="Convert text to images with Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/plain",
+    long_description="Visit https://github.com/5omega/utfcolor for more information.",
     url="https://github.com/5omega/utfcolor",
     classifiers=[
         "Programming Language :: Python :: 3",
